575a80e97a8f1ddb7aec3bd4605e6a9dbdd44db1
tkimu3 <t20468616+tkimu3@users.noreply.github.com>
2023-03-11 11:38:11 +0900

    Complete 3-3

---
+++ chapter_03/3-2_greetings.py
@@ -0,0 +1,5 @@
+friends = ['Joey', 'Ross', 'Chandler', 'Rachel', 'Monica', 'Phoebe']
+
+for n in friends:
+    message = f'Hi, {n.title()}. How you doing?'
+    print(message)
---
+++ chapter_03/3-3_your_own_list.py
@@ -0,0 +1,6 @@
+mortorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+message = f"I would like to buy a {mortorcycles[-1].title()}, but I can't afford it because it's too expensive."
+print(message)
+
+message = f"I will buy a {mortorcycles[1].title()}, because I can affort it"
+print(message)
